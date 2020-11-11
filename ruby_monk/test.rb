5f7217cd00edf38da0d2acd1b477b0582dd861fc
bocalo <moldavskiym@yandex.ru>
2020-11-11 19:04:59 +0300

    Add a new file

--- ruby_monk/test.rb
+++ ruby_monk/test.rb
@@ -58,4 +58,48 @@ p describe([" Greg Lief", 42])
 
 # А где здесь ошибка?
 
+##########################################################
+
+
+class Food
+end
+
+class Soup < Food
+end
+class IceCream < Food
+end
+class ChineseGreenBeans < Food
+end
+
+class DeliveryTray
+  FOOD_DISH_MAPPING = {
+    Soup => "soup bowl",
+    IceCream => "ice cream bowl",
+    ChineseGreenBeans => "serving plate"
+  }
+
+  def initialize
+    @dishes_needed = {}
+  end
+
+  def add(food)
+    dish = FOOD_DISH_MAPPING[food.class]
+    @dishes_needed[dish] = (@dishes_needed[dish] || 0) + 1
+  end
+
+  def dishes_needed
+    return "None." if @dishes_needed.empty?
+
+    @dishes_needed.map {|dish_name, count| "#{count} #{dish_name}" }.join(", ")
+  end
+end
+
+
+d=DeliveryTray.new
+d.add Soup.new; d.add Soup.new
+d.add IceCream.new
+puts d.dishes_needed # should be "2 soup bowl, 1 ice cream bowl"
+
+# А как можно посмотреть, что происходит внутри @dishes_needed.map  стр №93
+
 

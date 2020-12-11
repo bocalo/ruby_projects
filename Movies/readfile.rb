c05f45e9341dac5d303186dc43925fa81eb7659b
bocalo <moldavskiym@yandex.ru>
2020-12-11 19:41:38 +0300

    Add some changes

--- Movies/readfile.rb
+++ Movies/readfile.rb
@@ -1,65 +1,31 @@
 # file = ARGV(0)
-
 # puts "Opening file:#{file}"
-
 # myFile = File.open(file)
 # txt = myFile.read()
 # puts txt
 
-
-
-# file = File.open("movies.txt", "r")
-#   puts file.read
-# file.close()
-
-
-
-
-
-
-
-
-
-
-# File.open("movies.txt", "r") do |file|
-#   puts file.read()
-# end
-
-# file = File.open("movies.txt", "r")
-# puts file.read
-# file.close()
-
-
-# lines = []
-# file = File.open("movies.txt", "r")
-
-# while (line = file.gets)
-#   lines << line if line.include?("Max")
-# end
-# file.close
-
-# lines.each { |l| puts l }
-
-lines = []
-
-file = File.open("movies.txt", "r")
 movies = []
 
-while (line = file.gets)
-  arr = line.split("|")
-  if arr[1].include?("Max")
-    movies.push({title: arr[1], raiting: arr[7] })
+file = File.read("movies.txt")
+line = file.split("\n")
+
+
+line.each do |el|
+  movie = el.split("|")
+  if movie[1].include?("Max") 
+    movies.push({title: movie[1], raiting: movie[7]})
   end
-  if arr[7] > 8
-    ((arr[7] - 8) * 10).round(1) 
-  end
-  movies
 end
 
-file.close
+ x = movies.map { |movie| movie[:title] }
+ y = movies.map { |movie| movie[:raiting] }
 
-x = movies.map { |movie| movie[:title] }
-y = movies.map { |movie| movie[:raiting] }
+ print x, y
+ 
 
-puts x
-puts y 
+# if movie[7].to_i > 8
+#   movie[7] = (movie[7].to_i - 8).round(1) * 10 * "*"
+# end
+
+ 
+

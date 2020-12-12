c016228d9c3e177c0d9bd06fa88c82b3371fe0ad
bocalo <moldavskiym@yandex.ru>
2020-12-12 10:34:11 +0300

    Add new changes

--- Movies/readfile.rb
+++ Movies/readfile.rb
@@ -9,23 +9,28 @@ movies = []
 file = File.read("movies.txt")
 line = file.split("\n")
 
-
 line.each do |el|
   movie = el.split("|")
-  if movie[1].include?("Max") 
-    movies.push({title: movie[1], raiting: movie[7]})
+  if movie[1].include?("Max")
+    movies.push({ title: movie[1], raiting: movie[7] })
   end
+  movies.each do |f|
+    puts "This movie #{f[:title]} has raiting #{f[:raiting]}"
+    # rate = f[:rating]
+    # acc = ((rate - 8) * 10).to_s
+    # stars = acc.times * "*"
+    # rate = f[:stars]
+    # puts "This movie #{f[:title]} has raiting #{f[:stars]}"
+  end
+
+  movies
 end
 
- x = movies.map { |movie| movie[:title] }
- y = movies.map { |movie| movie[:raiting] }
+x = movies.map { |movie| movie[:title] }
+y = movies.map { |movie| movie[:raiting] }
 
- print x, y
- 
+print x, y
 
 # if movie[7].to_i > 8
 #   movie[7] = (movie[7].to_i - 8).round(1) * 10 * "*"
 # end
-
- 
-

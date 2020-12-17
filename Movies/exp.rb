36587972309246060b28962e70d63a6c7a3ba6f7
bocalo <moldavskiym@yandex.ru>
2020-12-17 22:16:53 +0300

    Add final changes

--- Movies/exp.rb
+++ Movies/exp.rb
@@ -6,19 +6,19 @@ movies = lines.map { |line| movie = line.split("|")
    {title: movie[1], country: movie[3], output: movie[4], genre: movie[5], duration: movie[6], director: movie[8]}
 }
 
-def print_movies(comedy)
-   print "Print date of comedy: #{comedy} "
-end
-comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(5)
-print_movies(comedy)
-
-
 def print_movies(movies)
-   movies.each { |title| print "The movie has title #{title} and #{comedy} and #{duration} and #{output} and #{total} and #{director} and #{long}" }
+   movies.each do |movie|
+     puts "This movie: #{movie[:title]}, date of output: (#{movie[:output]}), director of the movie: #{movie[:director]} and movie's duration - #{movie[:duration]}. Near forgot: this is #{movie[:genre]} and this movie is from #{movie[:country]}." 
+   end
 end
+
+new_com = movies.select { |el| el if el[:genre].include?("Comedy") }.compact.take(10)
+ print_movies(new_com)
+
 output = movies.map { |el| el[:output] }.take(10)
 title = movies.map { |el| el[:title] }.take(5)
-comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(5)
+comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(10)
+new_com = movies.select { |el| el[:genre].include?("Comedy") }.compact.take(10)
 total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
 long = movies.map { |el| el[:duration].to_i }.sort.reverse.take(5)
 director = movies.map { |el| el[:director] }.uniq
@@ -26,53 +26,4 @@ arr = director.sort_by do |el|
    el.split[1]
 end
 arr.take(10)
-
-print_movies(movies)
-print_movies(total)
-print_movies(output)
-print_movies(title)
-print_movies(comedy)
-print_movies(director)
-
-
-# title = movies.map { |el| el[:title] }.take(5)
-# output = movies.map { |el| el[:output] }.take(10)
-# long = movies.map { |el| el[:duration].to_i }.sort.reverse.take(5)
-# director = movies.map.sort_by { |el| el[:director].split(' ').last }.uniq
-# director = movies.map { |el| el[:director] }.uniq
-# arr = director.sort_by do |el|
-#    el.split[1]
-# end
-# arr.take(10)
-# total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
-
-#output = movies.map { |el| puts "#{el[:title]}: #{(el[:output])}; #{el[:genre]} - #{el[:duration]} and director: #{el[:director]}" }.take(5)
-
-#output = movies.map { |el| puts "#{el[:title]}: #{el[:output]}: #{el[:comedy]}" }
-
-# def print_info(movies)
-#     movies.split(' ').each { |el| p el} 
-# end
-
-# print_info("The Night of the Hunter: 1955-11-24; Crime,Drama,Film-Noir - 92 min and director: Charles Laughton")
-
-
-
-# def print_info(arr)
-#   print "Print numbers: #{arr}"
-# end
-
-# print_info([1, 2, 3])
-
-
-# def print_info()
-#     p country
-# end
-
-# print_info("The Night of the Hunter: 1955-11-24; Crime,Drama,Film-Noir - 92 min and director: Charles Laughton")
-
-# def print_movies(comedy)
-#    print "The movies #{comedy} "
-# end
-# comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(5)
 

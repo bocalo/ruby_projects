118a4a3fc592817ccac46305413bdfd9fef01409
bocalo <moldavskiym@yandex.ru>
2020-12-17 19:01:47 +0300

    Add changes

--- Movies/exp.rb
+++ Movies/exp.rb
@@ -6,26 +6,55 @@ movies = lines.map { |line| movie = line.split("|")
    {title: movie[1], country: movie[3], output: movie[4], genre: movie[5], duration: movie[6], director: movie[8]}
 }
 
+def print_movies(comedy)
+   print "Print date of comedy: #{comedy} "
+end
+comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(5)
+print_movies(comedy)
 
-comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact
 
+def print_movies(movies)
+   movies.each { |title| print "The movie has title #{title} and #{comedy} and #{duration} and #{output} and #{total} and #{director} and #{long}" }
+end
 output = movies.map { |el| el[:output] }.take(10)
-
+title = movies.map { |el| el[:title] }.take(5)
+comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(5)
+total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
 long = movies.map { |el| el[:duration].to_i }.sort.reverse.take(5)
-
-director = movies.map.sort_by { |el| el[:director].split(' ').last }.uniq
 director = movies.map { |el| el[:director] }.uniq
 arr = director.sort_by do |el|
    el.split[1]
 end
+arr.take(10)
 
-total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
+print_movies(movies)
+print_movies(total)
+print_movies(output)
+print_movies(title)
+print_movies(comedy)
+print_movies(director)
 
-output = movies.map { |el| puts "#{el[:title]}: #{(el[:output])}; #{el[:genre]} - #{el[:duration]} and director: #{el[:director]}" }.take(5)
 
-#output = movies.map { |el| puts "#{el[:title]}: #{el[:output]}; #{el[:comedy]}" }
+# title = movies.map { |el| el[:title] }.take(5)
+# output = movies.map { |el| el[:output] }.take(10)
+# long = movies.map { |el| el[:duration].to_i }.sort.reverse.take(5)
+# director = movies.map.sort_by { |el| el[:director].split(' ').last }.uniq
+# director = movies.map { |el| el[:director] }.uniq
+# arr = director.sort_by do |el|
+#    el.split[1]
+# end
+# arr.take(10)
+# total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
 
-# print_info([{title: movie[1], country: movie[3], output: movie[4], genre: movie[5], duration: movie[6], director: movie[8]}])
+#output = movies.map { |el| puts "#{el[:title]}: #{(el[:output])}; #{el[:genre]} - #{el[:duration]} and director: #{el[:director]}" }.take(5)
+
+#output = movies.map { |el| puts "#{el[:title]}: #{el[:output]}: #{el[:comedy]}" }
+
+# def print_info(movies)
+#     movies.split(' ').each { |el| p el} 
+# end
+
+# print_info("The Night of the Hunter: 1955-11-24; Crime,Drama,Film-Noir - 92 min and director: Charles Laughton")
 
 
 
@@ -36,13 +65,14 @@ output = movies.map { |el| puts "#{el[:title]}: #{(el[:output])}; #{el[:genre]} 
 # print_info([1, 2, 3])
 
 
-def print_info(movies)
-  print "#{total}"
-end
+# def print_info()
+#     p country
+# end
 
-print_info("http://imdb.com/title/tt0111161/?ref_=chttp_tt_1|The Shawshank Redemption|1994|USA|1994-10-14|Crime,Drama|142 min|9.3|Frank Darabont|Tim Robbins,Morgan Freeman,Bob Gunton
-http://imdb.com/title/tt0068646/?ref_=chttp_tt_2|The Godfather|1972|USA|1972-03-24|Crime,Drama|175 min|9.2|Francis Ford Coppola|Marlon Brando,Al Pacino,James Caan
-http://imdb.com/title/tt0071562/?ref_=chttp_tt_3|The Godfather: Part II|1974|USA|1974-12-20|Crime,Drama|200 min|9.1|Francis Ford Coppola|Al Pacino,Robert De Niro,Robert Duvall"
-)
+# print_info("The Night of the Hunter: 1955-11-24; Crime,Drama,Film-Noir - 92 min and director: Charles Laughton")
 
+# def print_movies(comedy)
+#    print "The movies #{comedy} "
+# end
+# comedy = movies.map { |el| el[:genre] if el[:genre].include?("Comedy") }.compact.take(5)
 

7f3fdf8cc8c71306e420948b73fa0b6dec2a4f20
bocalo <moldavskiym@yandex.ru>
2020-12-22 17:40:24 +0300

    Add some changes

--- Movies/struct.rb
+++ Movies/struct.rb
@@ -1,6 +1,11 @@
 require "csv"
+require "ostruct"
 
 movies = CSV.read("movies.txt", headers: [:url, :title, :date, :country, :output, :genre, :duration, :rating, :director, :actors], col_sep: "|", converters: :numeric)
+
+film = movies.map do |movie|
+  movie = OpenStruct.new(movie)
+end
 
 def print_movies(movies)
   movies.each do |movie|
@@ -8,29 +13,16 @@ def print_movies(movies)
   end
 end
 
-#pp movies[0]
-# pp movies[0][:title]
-# pp movies[0][:url]
-# pp movies[0][:country]
-# pp movies[0][:output]
-# pp movies[0][:genre]
-# pp movies[0][:duration]
-# pp movies[0][:director]
-
 new_long = movies.sort_by { |el| el[:duration].to_i }.reverse.take(5)
-#pp new_long
 new_com = movies.select { |el| el[:genre].include?("Comedy") }.compact.take(10)
-#pp new_com
 url = movies.map { |el| el[:url] }.uniq
-#pp url
 director = movies.map { |el| el[:director] }.uniq
-#p director
 arr = director.sort_by { |el| el.split[1] }.take(10)
-#p arr
-# puts "The list of 10 famous directors: #{arr}"
-# total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
-# puts "The amount of the movies not from USA is: #{total}"
 
-#print_movies(new_long)
-#print_movies(new_com)
-#print_movies(new_long)
+puts "The list of 10 famous directors: #{arr}"
+total = movies.map { |el| el[:country] unless el[:country].include?("USA") }.compact.count
+puts "The amount of the movies not from USA is: #{total}"
+
+print_movies(new_long)
+print_movies(new_com)
+print_movies(new_long)
--- Movies/test.rb
+++ Movies/test.rb
@@ -1,26 +1,5 @@
 require "csv"
-movies = CSV.read("movies.txt", headers: [:url, :title, :date, :country, :output, :genre, :duration, :rating, :director, :actors], col_sep: "|", converters: :numeric)
-
-# CSV.foreach("movies.txt", col_sep: "|", converters: :numeric) do |row|
-#   p row[1]
-# end
-#pp movies[0]
-#pp movies[0][:title]
-# pp movies[0][:url]
-# pp movies[0][:date]
-# pp movies[0][:country]
-# pp movies[0][:output]
-# pp movies[0][:genre]
-# pp movies[0][:duration]
-# pp movies[0][:rating]
-# pp movies[0][:director]
-# pp movies[0][:actors]
-# pp movies[0][:director]
-
-# title = movies[0][1]
-# pp title
-
-# Movies = OpenStruct.new(:title, :country, :output, :genre, :duration, :director)
+require "ostruct"
 
 # file = File.read("movies.txt")
 # lines = file.split("\n")
@@ -30,14 +9,16 @@ movies = CSV.read("movies.txt", headers: [:url, :title, :date, :country, :output
 #   { title: movie[1], country: movie[3], output: movie[4], genre: movie[5], duration: movie[6], director: movie[8] }
 # }
 
+# movies = CSV.read("movies.txt", headers: [:url, :title, :date, :country, :output, :genre, :duration, :rating, :director, :actors], col_sep: "|", converters: :numeric)
+
 # def print_movies(movies)
 #   movies.each do |movie|
 #     puts "\nThis movie: #{movie[:title]}, date of output: (#{movie[:output]}), director of the movie: #{movie[:director]} and movie's duration - #{movie[:duration]}. Near forgot: this is #{movie[:genre]} and this movie is from #{movie[:country]}."
 #   end
 # end
 
-new_long = movies.sort_by { |el| el[:duration].to_i }.reverse.take(5)
-pp new_long
+# new_long = movies.sort_by { |el| el[:duration].to_i }.reverse.take(5)
+# pp new_long
 # new_com = movies.select { |el| el[:genre].include?("Comedy") }.compact.take(10)
 # director = movies.map { |el| el[:director] }.uniq
 # arr = director.sort_by { |el| el.split[1] }.take(10)
